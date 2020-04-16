9a34dc44fcc94f88766462dc6936d66d458a3cdd
winter520 <WDwq681227>
2020-04-16 16:47:48 +0800

    optimized nun code

--- server/public/methods/methods.js
+++ server/public/methods/methods.js
@@ -97,8 +97,14 @@ function fixPkey (key) {
 }
 
 function createSixNum () {
-  let num = Math.ceil(Math.random() * 1000000)
-  return num.toString()
+  let n1 = Math.ceil(Math.random() * 10),
+      n2 = Math.ceil(Math.random() * 10),
+      n3 = Math.ceil(Math.random() * 10),
+      n4 = Math.ceil(Math.random() * 10),
+      n5 = Math.ceil(Math.random() * 10),
+      n6 = Math.ceil(Math.random() * 10)
+  let num = '' + n1 + n2 + n3 + n4 + n5 + n6
+  return num
 }
 
 module.exports = {
