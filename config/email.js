10b181ed8be46084ff149b14a8fd5731f9db7f6e
winter520 <WDwq681227>
2020-04-16 15:34:05 +0800

    change email config

--- config/email.js
+++ config/email.js
@@ -5,9 +5,12 @@ let host = {
 
 module.exports = {
   host: 'smtp.163.com', 
+  // host: '220.181.72.147',
+  // host: 'smtp.mxhichina.com', 
+  // port: 25,
   port: 465,
   secureConnection: true,
-  secure:false,
+  // secure:false,
   auth: {
     user: 'fusion15627@163.com', //刚才注册的邮箱账号
     pass: 'ISEZPUGXMMYDCPOS'  //邮箱的授权码，不是注册时的密码
